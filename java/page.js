ddb713aa3a04d78d46a87b06469ef8de4a5781ce
shaemc <car16037@byui.edu>
2021-04-27 20:01:29 -0600

    updates for assignment page

--- java/page.js
+++ java/page.js
@@ -2,6 +2,6 @@ window.addEventListener('load', (event)=>{
     const lu = document.querySelector('#lastupdated');
     lu.textContent = document.lastModified;
 
-    const cry = document.querySelector('#year');
+    const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
 })
