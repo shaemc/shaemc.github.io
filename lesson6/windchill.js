98fb51d1c33a414eadb761918a770103e15d4f30
shaemc <car16037@byui.edu>
2021-06-02 10:37:09 -0600

    fixed windchill

--- lesson6/windchill.js
+++ lesson6/windchill.js
@@ -1,8 +1,11 @@
 window.addEventListener("load", () => {
 
-    var temp = parseFloat(document.getElementById('temp').value);
-    var speed = parseFloat(document.getElementById('windspeed').value);
+    var temp = parseFloat(document.getElementById('temp').textContent);
+    var speed = parseFloat(document.getElementById('windspeed').textContent);
     var windchillfactor = (35.74 + (0.6215 * temp) - (35.75 * (speed ** 0.16)) + (0.4275 * temp * (speed ** 0.16))).toFixed(0);
     document.querySelector('#windchill').innerHTML = `${Number(windchillfactor)} °F`;
-
+    console.log(temp)
+    console.log(speed)
+    console.log(windchillfactor)
+    console.log(document.querySelector('#windchill'))
 });
