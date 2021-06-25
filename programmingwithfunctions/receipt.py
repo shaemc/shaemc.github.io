77c105f28798fa262456275726fc1bb6303c0705
shaemc <car16037@byui.edu>
2021-06-25 15:08:23 -0600

    programming with functions

--- programmingwithfunctions/receipt.py
+++ programmingwithfunctions/receipt.py
@@ -1,15 +1,25 @@
 import csv
 
+from datetime import datetime
+
+currentdatetime = datetime.now()
+storename = 'Inkom Emporium'
+
 def main():
     NUMBER_INDEX = 0
     NAME_INDEX = 1
     PRICE = 2
 
+    print()
+    print(storename)
     products = read_products('products.csv', NUMBER_INDEX)
-    print(products)
     print()
     process_request('request.csv', NUMBER_INDEX, products)
-    
+
+    print()
+    print(f'Thank you for shopping at {storename}.')
+    print(f"{currentdatetime:%A %I:%M %p}")
+    print()
 
 def read_products(filename, key_column_index):
 
@@ -36,20 +46,37 @@ def process_request(filename, key_column_index, products):
         
         next(process)
         
-        print('Requested Items')
+        items = []
+        subtotal = []
+
         for row in process:
             key = row[key_column_index]
             dictionary[key] = row[1]
-        
-            
-            #for row in products:
             name = products[key][0]
             price = products[key][1]
-            #request = dictionary[key]
             many = dictionary[key]
+            
             print(f"{name}: {many} @ {price}")
 
-    #return
+            items.append(float(many))
+            fun = float(price) * float(many)
+            subtotal.append(float(fun))
+        
+        sumlist = float(sum(items)) 
+        sumsubtotal = float(sum(subtotal))  
+        tax = float(sumsubtotal) * .06
+        total = float(sumsubtotal) + tax
+
+        print()
+        print(f'Number of items: {sumlist:.0f}')
+        print(f'Subtotal: {sumsubtotal}')
+        print(f'Sales tax: {tax:.2f}')
+        print(f'Total: {total:.2f}')
+
+        payment = input ('What is the payment amount? $')
+        change = (float(payment) - float(total))
+        print (f'Change: ${change:.2f}')
+        print ()
 
 if __name__ == "__main__":
     main()
