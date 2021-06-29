9024aefeb9a68b612fc033b9f083390dec1718c1
shaemc <car16037@byui.edu>
2021-06-29 16:25:52 -0600

    we have icons people!

--- lesson10/js/prestonweather.js
+++ lesson10/js/prestonweather.js
@@ -1,6 +1,6 @@
 // 5604473
 
-const requestURL = ' https://api.openweathermap.org/data/2.5/weather?zip=83263&appid=a5a58e084b9faac42b062f6e4dfc63ce&units=imperial'
+const requestURL = 'https://api.openweathermap.org/data/2.5/weather?zip=83263&appid=a5a58e084b9faac42b062f6e4dfc63ce&units=imperial'
 
 fetch(requestURL)
   .then(function (response) {
@@ -51,27 +51,58 @@ fetch(requestURL)
     document.querySelector('div.cards').appendChild(card);
     });
 
-fetch(requestURL)
+const requestURL2 = 'https://api.openweathermap.org/data/2.5/weather?zip=83263&appid=a5a58e084b9faac42b062f6e4dfc63ce&units=imperial'
+
+fetch(requestURL2)
     .then(function (response) {
         return response.json();
     })
     .then(function (five) {
         console.table(five); 
 
-        let fiveday = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let p = document.createElement('p');
-        let icon = document.createElement('img');
-            
+        // const days = [
+        //   "Saturday",
+        //   "Sunday",
+        //   "Monday",
+        //   "Tuesday",
+        //   "Wednesday",
+        //   "Thursday",
+        //   "Friday",
+        // ];
 
-        h2.textContent = five.name;
-        p.textContent = five.main.temp;
-        icon.setAttribute('src', `https://openweathermap.org/img/${five.weather[0].icon}.jpg`); //this is wrong
+        let temp1 = document.querySelector('#temp1');
+        let temp2 = document.querySelector('#temp2');
+        let temp3 = document.querySelector('#temp3');
+        let temp4 = document.querySelector('#temp4');
+        let temp5 = document.querySelector('#temp5');
+
+        let icon1 = document.querySelector('#icon1');
+        let icon2 = document.querySelector('#icon2');
+        let icon3 = document.querySelector('#icon3');
+        let icon4 = document.querySelector('#icon4');
+        let icon5 = document.querySelector('#icon5');
+
+        // temp1.textContent = five.day[0].weather[0].main.temp;
+        // temp2.textContent = five.weather[1].main.temp;
+        // temp3.textContent = five.weather[2].main.temp;
+        // temp4.textContent = five.weather[3].main.temp;
+        // temp5.textContent = five.weather[4].main.temp;
+
+        icon1.setAttribute('src', `https://openweathermap.org/img/wn/${five.weather[0].icon}@2x.png`);
+        icon2.setAttribute('src', `https://openweathermap.org/img/wn/${five.weather[0].icon}@2x.png`);
+        icon3.setAttribute('src', `https://openweathermap.org/img/wn/${five.weather[0].icon}@2x.png`);
+        icon4.setAttribute('src', `https://openweathermap.org/img/wn/${five.weather[0].icon}@2x.png`);
+        icon5.setAttribute('src', `https://openweathermap.org/img/wn/${five.weather[0].icon}@2x.png`);
         
-
-        fiveday.appendChild(h2);
-        fiveday.appendChild(p);
-        fiveday.appendChild(icon);
-
-        document.querySelector('div.fiveday').appendChild(fiveday);
+        // let fiveday = document.createElement('section');
+        // let h2 = document.createElement('h2');
+        // let p = document.createElement('p');
+        // let icon = document.createElement('img');
+         
+        // h2.textContent = five.name;
+        // p.textContent = five.main.temp;   
+         // fiveday.appendChild(h2);
+        // fiveday.appendChild(p);
+        // fiveday.appendChild(icon);
+        // document.querySelector('div.fiveday').appendChild(fiveday);
     });
